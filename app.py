ea6814cdc6b834666395c33a4430b8e97cda6bb7
Yegor Smertenko <yegorsmertenko@Yegors-MacBook-Pro.local>
2023-07-19 20:40:43 -0700

    add med screen sizing

--- app.py
+++ app.py
@@ -306,6 +306,7 @@ app.layout = dbc.Container(
                         "padding-left": "20px",
                         "padding-right": "30px",
                     },
+                    md=6,
                     xs=12,
                 ),
             ],
