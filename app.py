f3fcd83ba1399e25ee5816251693b9d2473e4a3b
Caps1d <ye.smertenko@gmail.com>
2023-09-05 21:11:52 -0700

    add full methodology walkthrough and reformatting methodology in web app

--- app.py
+++ app.py
@@ -332,6 +332,60 @@ app.layout = dbc.Container(
         ),
         dbc.Row(
             [
+                html.Div(
+                    [
+                        html.P(
+                            "Project Overview",
+                            style={
+                                "text-align": "center",
+                                "align-items": "center",
+                                "color": "#DCDCDC",
+                                "marginBottom": "10px",
+                                "fontSize": "34px",
+                                "font-weight": "bold",
+                            },
+                        ),
+                    ]
+                ),
+                html.Div(
+                    [
+                        html.P(
+                            "The aim of this project was to provide a hazard score for all locations in Ukraine that were involved in war-related events such battles, explosions/remote violence events, violence against civilians and factors that pose risk to civilians living in those locations. The idea is to take those factors and condense them into one readable and comprehensible score which would reflect the danger level of the associated location.",
+                            style={"marginBottom": "20px"},
+                        ),
+                        html.P(
+                            "The main issue I have faced while developing a statistical method that could provide such a score was to combine domain knowledge and account for the known unknowns. In complex events like wars, it's very difficult to obtain a comprehensive dataset that can capture the underlying dynamics. Not because of lack of data but due to the abundance and sheer number of factors involved that are either too hard or too expensive to account for. Furthermore, the issue of inherent bias involved in various sources is also prevalent and poses a serious risk of polluting a dataset. Hence, besides accounting for all factors that contribute to an event, one must also consider how valid the data that they’re using is. Therefore, I decided to use the data provided to me by ACLED for non-commercial purposes, the aim of this project is for data exploration, visualization and awareness. ACLED is a non-profit organization that has been collecting data on armed conflict events since 2014. For further information on ACLED please refer to the hyperlink above. ",
+                            style={"marginBottom": "20px"},
+                        ),
+                        html.P(
+                            "ACLED’s raw dataset was comprehensive enough to include three levels of administrative borders for each location, event types, sub events, dates, casualties, presence of civilian targeting, what actors were involved in the event, and a text based note column that included additional information pertaining to the event. As mentioned before, due the complexity and nature of armed conflicts there are a significant number of factors that cannot be accounted for and some are just not available to the public. Namely for this project, ideally, I would need access to air defense numbers in a location, overall equipment concentration, the number of missiles that have been launched in a remote violence event, where they were launched from, how many have been shot down, logistic routes and hubs and this is just to name a few. Some of this data is available to the public, but the sources are sparse and inconsistent. In other words, the known unknowns. Hence, for now I have decided to use the baseline data set from ACLED and process the data enough to account for some of those factors I mentioned earlier.",
+                            style={"marginBottom": "20px"},
+                        ),
+                        html.P(
+                            "In later updates to this project, I aim to include some new features such as whether a location is part of a logistic route, missile count and type and reduce the number of hardcoded points that are used to account for domain knowledge on the war in Ukraine. Furthermore, for people interested in numbers, I will also include various plots to show event distributions, frequencies and more visualizations of various relationships that are present in the dataset. Nevertheless, I am happy with how this project has turned out and its accuracy in reflecting the danger level of locations across Ukraine.",
+                            style={"marginBottom": "20px"},
+                        ),
+                        html.P(
+                            "Quick note on the project's implementation: everything was done using python and various data processing libraries, numpy and pandas being at the core of it and plotly’s dash was used for making the web app.",
+                        ),
+                    ],
+                    style={
+                        "text-align": "left",
+                        "align-items": "left",
+                        "color": "#DCDCDC",
+                        "padding-left": "40px",
+                        "padding-right": "40px",
+                        "fontSize": "18px",
+                    },
+                ),
+            ],
+            style={
+                "justify-content": "center",
+                "padding-top": "80px",
+            },
+        ),
+        dbc.Row(
+            [
                 dbc.Col(
                     [
                         html.P(
@@ -347,9 +401,12 @@ app.layout = dbc.Container(
                             },
                         ),
                     ]
-                )
+                ),
             ],
-            style={"padding-top": "80px"},
+            style={
+                "padding-top": "40px",
+                "padding-bottom": "6px",
+            },
         ),
         html.Div(
             [
@@ -357,35 +414,28 @@ app.layout = dbc.Container(
                     [
                         dcc.Markdown(
                             """
-                    &nbsp;&nbsp;&nbsp;&nbsp;The generation of the hazard_score variable was a process that involved several stages of data collection, preprocessing, feature engineering, and dimensional reduction.
-                    My goal was to create a comprehensive hazard score that could reflect the danger level in different regions of Ukraine, based on geolocated conflict data.
+                            First, in order to obtain the raw dataset I had to write a python script that uses ACLED’s API and a list of query filters that will filter out any data that I find irrelevant for this project or the dataset batch I am working on. I made different batches of data since the events in Ukraine would also develop in separate phases. For example, UAF’s (Ukrainian Armed Forces) summer offensive in 2022 which lead to the reclamation of a significant portion of the Kharkiv region presents a different state of events than what happened during the winter of 2023 with Russia’s winter offensive and the battle for Bakmut involving Wagner pmc’s. Furthermore, some data like iso (country code of the event), event id, and a few other id columns were filtered out from the dataset due to their irrelevance for the task at hand alongside with source type (source of the event report), source scale, and inter 1 and inter2. The reason for dropping some of the non-id related columns was due to them not contributing to the actual relationships between the data I was exploring and some features that were included already carry the context of those dropped features.
                 """,
                             style={"marginBottom": "20px"},
                         ),
                         html.P(
-                            [
-                                "The process began with data collection. I gathered data on conflict-related events in Ukraine, thanks to the Armed Conflict Location & Event Data Project (ACLED). ",
-                                html.A(
-                                    " ACLED",
-                                    href="https://acleddata.com/",
-                                    target="_blank",
-                                ),
-                                " is a highly respected organization providing real-time data and analysis on current political violence and protest across the world. I am grateful for their support"
-                                " in making this data available to me free of charge. Their data on the location, number of casualties, number of shelling events were"
-                                " invaluable for this analysis. These factors were carefully chosen because they have a direct impact on the danger level of a region.",
-                            ],
+                            "The obtained filtered dataset includes the following baseline features (i.e columns): event date, event type (Battles, Explosions/Remote violence, Violence against civilians), sub event type (armed clashes, shelling/artillery/missile attack, etc.), actor1, actor2, admin1, admin2, admin3 (the 3 admin columns are representing 3 levels of administrative boundaries of the location), location, latitude, longitude, notes (short description of the event), fatalities, civilian targeting (whether civilians were the primary target or not).",
                             style={"marginBottom": "20px"},
                         ),
                         html.P(
-                            "The collected data was then preprocessed. Using Python libraries such as NumPy and pandas, I cleaned, processed,"
-                            " and formatted the data into a suitable structure for further analysis. This included handling missing values, converting data types,"
-                            " and standardizing the data.",
+                            "As my first step in data preprocessing I have read the json dataset and converted it into a dataframe using pandas (think of it like an excel sheet). I have then explored the datatypes of all columns and converted fatalities, latitude and longitude to numeric type. Then after exploring the dataset, I noticed that the notes column would sometimes include data on casualties. This was an important finding since I knew that some events actually had a significant number of civilian casualties that weren’t counting as fatalities and overall a significant number was never reported either, due to the lack of clarity in numbers varying between sources. Hence, even when some severe events like missile strikes or battles have occurred that had large numbers of casualties reported by external sources, the raw dataset would have 0 fatalities. In order to account best for casualties and affected parties, I have opted-in to try and extract this data from the text-based notes column. Due to varying formatting and wording something like regular expressions could not be used as a lot of the entries would get ignored due to formatting. Therefore, I have used SpaCy’s NLP machine learning model to do the trick. After setting the proper conditions for the token, the model was able to extract wounded data from the notes column and thus I created a new feature called ‘wounded’ which represents the number of wounded individuals involved in the event.",
                             style={"marginBottom": "20px"},
                         ),
                         html.P(
-                            "During the feature engineering phase, I created new features from existing data that could improve the performance of my model. For instance,"
-                            " I used spaCy's Natural Language Processing (NLP) capabilities to extract wounded data from text."
-                            " This allowed me to create a more nuanced and granular measure of conflict impact in each location.",
+                            "Next step was to aggregate the data. As mentioned before, each row would have an event date and thus I thought it’s best to aggregate the data by month for a location to condense the data a bit. This led to the creation of 2 new aggregate features: number of events and total casualties.",
+                            style={"marginBottom": "20px"},
+                        ),
+                        html.P(
+                            "As mentioned before, there was a certain amount of domain knowledge that needed to be incorporated into the statistical approach designed for this project. Hence, I decided to create a coordinate list of locations that are well-known hotzones, such as Bakhmut, Kupiansk, Avdiivka, Vuhledar, Robotyne, and Soledar. The reasoning behind this is to use it as a reference point for a location's proximity to the battlefront. Those locations and a lot of others that neighbor them are known engagement zones which would have a hazard score of 100. In order to obtain the distance between a location and a hot zone I have used the haversine function which measures the distance between two sets of coordinates while accounting for earth’s curvature. After obtaining the distances of a location to each of the hot zones, I have selected the shortest distance out of all of them and created a new feature called minimal distance to the battlefront.",
+                            style={"marginBottom": "20px"},
+                        ),
+                        html.P(
+                            "Now that new features have been added, the next step was to encode some categorical features like event type and civilian targeting into numerical values using one-hot encoding. This way it's easier for the model to read the features as it can check whether a specific type of event has occurred simply by setting 1 or 0 in the respective column. Furthermore, I combined explosions/remote violence events with civilian targeting to differentiate between missile strikes on civilian infrastructure and shelling events at the frontline. The idea is that if civilian targeting has a true value in the same row where the explosion event is also registered then that means it's most likely a remote violence event caused by missile strikes on civilian infrastructure away from the frontline. The logic behind this is that it will be very unlikely that you will have a registered explosion event involving civilians at the front lines and thus any explosion/remote violence event that has false value for the civilian targeting column most likely points to an artillery event at the front lines, in other words, a high hazard zone. This was later confirmed by artillery sub event type in rows that have that event type and false for civilian targeting.",
                             style={"marginBottom": "20px"},
                         ),
                     ],
@@ -437,7 +487,11 @@ app.layout = dbc.Container(
                     },
                 ),
             ],
-            style={"padding-top": "50px"},
+            style={
+                # "padding-top": "20px",
+                "padding-left": "40px",
+                "padding-right": "40px",
+            },
             className="methodology",
         ),
         html.Script(
