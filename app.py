7a647ada13741f6709bb0722bf82cd51e982fc0a
Caps1d <ye.smertenko@gmail.com>
2023-09-08 12:57:30 -0700

    grammatical fix in project overview

--- app.py
+++ app.py
@@ -357,7 +357,7 @@ app.layout = dbc.Container(
                 html.Div(
                     [
                         html.P(
-                            "The aim of this project was to provide a hazard score for all locations in Ukraine that were involved in war-related events such battles, explosions/remote violence events, violence against civilians and factors that pose risk to civilians living in those locations. The idea is to take those factors and condense them into one readable and comprehensible score which would reflect the danger level of the associated location.",
+                            "The aim of this project was to provide a hazard score for all locations in Ukraine that were involved in war-related events such as battles, explosions/remote violence events, violence against civilians and factors that pose risk to civilians living in those locations. The idea is to take those factors and condense them into one readable and comprehensible score which would reflect the danger level of the associated location.",
                             style={"marginBottom": "20px"},
                         ),
                         dcc.Markdown(
